2f9124e0c4d17b8c60512330a3d4b4365f5e051d
anuanika21 <iamanuanika21@gmail.com>
2026-07-26 01:13:04 +0600

    Add validation for counter status

---
+++ src/counters/dto/update-status.dto.ts
@@ -0,0 +1,7 @@
+import { IsEnum } from 'class-validator';
+import { CounterStatus } from '../../common/enums/counter-status.enum';
+
+export class UpdateCounterStatusDto {
+  @IsEnum(CounterStatus)
+  status: CounterStatus;
+}
