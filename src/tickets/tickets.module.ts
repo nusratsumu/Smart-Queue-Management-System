775c2482a5a62ad186d4acb5c1bdd6ac2549572c
nusratsumu <nusratsumu2003@gmail.com>
2026-07-26 16:50:23 +0600

    register Tickets, Services, Queues, Counters and MailModule

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TicketsController } from './tickets.controller';
 import { TicketsService } from './tickets.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tickets } from './tickets.entity';
+import { Services } from '../services/services.entity';
+import { Queues } from '../queues/queues.entity';
+import { Counters } from '../counters/counters.entity';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tickets])],
+  imports: [
+    TypeOrmModule.forFeature([Tickets, Services, Queues, Counters]),
+    MailModule,
+  ],
   controllers: [TicketsController],
-  providers: [TicketsService]
+  providers: [TicketsService],
 })
 export class TicketsModule {}
