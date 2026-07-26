8425614da5bd8c6d2d94957c39db90e16c8f3180
Naorin-islam <naorinislamrupam6532@gmail.com>
2026-07-26 23:57:36 +0600

    implementing notification.service.ts file

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Notifications } from './notifications.entity';
+import { NotificationType } from '../common/enums/notification-type.enum';
+import { NotificationStatus } from '../common/enums/notification-status.enum';
 
 @Injectable()
-export class NotificationsService {}
+export class NotificationsService {
+    constructor(
+        @InjectRepository(Notifications)
+        private readonly notificationsRepository: Repository<Notifications>,
+      ) {}
+
+      async create(
+        userId: number,
+        type: NotificationType,
+        message: string,
+        status: NotificationStatus = NotificationStatus.SENT,
+      ): Promise<Notifications> {
+        const notification = this.notificationsRepository.create({
+          user: { id: userId } as any,
+          type,
+          message,
+          status,
+        });
+        return this.notificationsRepository.save(notification);
+      }
+
+      async findMyNotifications(userId: number): Promise<Notifications[]> {
+        return this.notificationsRepository.find({
+          where: { user: { id: userId } },
+          order: { sentAt: 'DESC' },
+          
+        });
+      }
+
+}
