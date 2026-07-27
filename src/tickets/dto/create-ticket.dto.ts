e8f2f369f2217cdcbbb176ef19bdb133c6b1c796
nusratsumu <nusratsumu2003@gmail.com>
2026-07-27 08:24:15 +0600

    added tickets require queueId on createticket dto, add callNext

--- src/tickets/dto/create-ticket.dto.ts
+++ src/tickets/dto/create-ticket.dto.ts
@@ -1,9 +1,13 @@
-import { IsInt, IsNotEmpty, IsOptional, IsPositive, IsString, MaxLength } from 'class-validator';
+import { IsInt, IsOptional, IsPositive, IsString, MaxLength } from 'class-validator';
 
 export class CreateTicketDto {
   @IsInt()
   @IsPositive()
   serviceId: number;
+
+  @IsInt()
+  @IsPositive()
+  queueId: number;
 
   @IsOptional()
   @IsString()
